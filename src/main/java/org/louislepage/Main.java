c12800c8ed5f7b41ed18db6c23c7c835fa2000f3
louislepage <l.lepage@campus.tu-berlin.de>
2023-06-25 12:44:56 +0200

    added dependency in mvn for systemds

---
+++ src/main/java/org/louislepage/Main.java
@@ -0,0 +1,10 @@
+package org.louislepage;
+
+import org.apache.sysds.runtime.matrix.data.MatrixBlock;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        MatrixBlock mb1 = new MatrixBlock();
+    }
+}
