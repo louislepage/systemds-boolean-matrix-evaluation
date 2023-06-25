00c6e8c21d487806d29dc00cd7399dcbcf28ee6f
louislepage <l.lepage@campus.tu-berlin.de>
2023-06-25 15:15:28 +0200

    added dependency in mvn for systemds

--- src/main/java/org/louislepage/Main.java
+++ src/main/java/org/louislepage/Main.java
@@ -1,10 +1,12 @@
 package org.louislepage;
 
+import org.apache.sysds.common.Types;
+import org.apache.sysds.runtime.data.DenseBlockFactory;
 import org.apache.sysds.runtime.matrix.data.MatrixBlock;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        MatrixBlock mb1 = new MatrixBlock();
+        MatrixBlock mb1 = new MatrixBlock(3,5, DenseBlockFactory.createDenseBlock(Types.ValueType.TRUE_BOOLEAN, new int[] {3,5}));
     }
 }
